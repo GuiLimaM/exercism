4eb28f05d967a477d512a164a6def37674163e35
GuiLimaM <guidorussas@hotmail.com>
2023-10-23 19:14:13 -0300

    Atualização da atividade 11
    Add 3 funções na atividade 11

--- JavaScritp/11elysesAnalyticEnchantmnts.js
+++ JavaScritp/11elysesAnalyticEnchantmnts.js
@@ -1,3 +1,15 @@
+const card = 3;
+
 function getCardPosition(stack, card) {
-    
+    return stack.findIndex((num) => num === card);
 }
+
+function doesStackIncludeCard(stack, card) {
+    return stack.some((num) => num === card);
+}
+
+function isEachCardEven(stack) {
+    return stack.every((num) => num % 2 === 0);
+}
+
+console.log(isEachCardEven([2, 4, 6, 7]));
